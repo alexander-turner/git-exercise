216cc3107df15ea0e655180e892af417c2cef8f8
alexander-turner <turneral1@grinnell.edu>
2013-09-03 10:29:33 -0500

    Update HelloWorld.java

--- src/edu/grinnell/csc207/turneral1/hello/HelloWorld.java
+++ src/edu/grinnell/csc207/turneral1/hello/HelloWorld.java
@@ -3,5 +3,6 @@ package edu.grinnell.csc207.turneral1.hello;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Punk'd!");
+        System.out.println("New output");
     } // main(String[])
 }
